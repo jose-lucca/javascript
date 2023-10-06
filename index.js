5220544fbbb6c19ea685dbe295617303bab14b26
jose-lucca <mrjoshejoshe@gmail.com>
2023-10-05 23:56:24 -0300

    practicaJavaScript1

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log ("Hola a todos");
+
+let numero1, numero2;
+numero1 = 10;
+numero2 = 20;
+
+console.log ("La suma de los dos numeros es: " + (numero1 + numero2));
+
+let objetoPersona = {nombre: "Jose", Apellido: "Lucca", Edad: 44, Carrera: "Full Stack Java"}
+
+console.log (objetoPersona);
+console.log (objetoPersona.nombre);
+
+/* Ingreso de datos */
+
+let nombrePersona, edad_A;
+
+nombrePersona = prompt ("Ingrese su Nombre:");
+edad_A = parseInt (prompt ("Ingresa tu edad:"));
+
+console.log ("Tu nombre es: " + nombrePersona + " y tu edad es: " + edad_A);
